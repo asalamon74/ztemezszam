82469eedc798b86904cb8819d87438ac18bb17d4
Andras Salamon <andras.salamon@melda.info>
2017-10-03 08:53:37 +0200

    replace deprecated code

--- app/src/main/java/info/melda/sala/zetemezszam/PrefsActivity.java
+++ app/src/main/java/info/melda/sala/zetemezszam/PrefsActivity.java
@@ -2,16 +2,25 @@ package info.melda.sala.zetemezszam;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 /**
  *
  */
 public class PrefsActivity extends PreferenceActivity {
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.prefs);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsPreferenceFragment()).commit();
+    }
+
+    public static class PrefsPreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.prefs);
+        }
     }
 }
